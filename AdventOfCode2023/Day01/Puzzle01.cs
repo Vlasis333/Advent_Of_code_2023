e989e59d4a6ddfceb051e4e4b31a83740f192a28
Vlasios Mavraganis <vlasisvv@gmail.com>
2023-12-01 21:35:19 +0200

    Update on puzzle01 to make it more generic to use as a back bone for next puzzles

--- AdventOfCode2023/Day01/Puzzle01.cs
+++ AdventOfCode2023/Day01/Puzzle01.cs
@@ -5,34 +5,31 @@
         public void Calculation()
         {
             // For you to work just by copy paste you need to get your own input and paste it to a txt file (named input daa :P)
-            string[] filePath = ReadCalibrationFile("C:\\Users\\vlasi\\Downloads\\input.txt");
+            string[] filePath = ReadFile("C:\\Users\\vlasi\\Downloads\\input.txt");
 
-            int totalCalibration = CalculateTotalCalibration(filePath);
-            Console.WriteLine($"Day 01 Puzzle 01 Result: {totalCalibration}");
-
-            // To keep the command line open
-            Console.ReadLine();
+            int finalAnswer = CalculateAnswer(filePath);
+            Console.WriteLine($"Day 01 Puzzle 01 Result: {finalAnswer}");
         }
 
-        private static string[] ReadCalibrationFile(string filePath)
+        private static string[] ReadFile(string filePath)
         {
             return File.ReadAllLines(filePath);
         }
 
-        private int CalculateTotalCalibration(string[] calibrationDocument)
+        private int CalculateAnswer(string[] fileData)
         {
-            int totalCalibration = 0;
+            int totalValue = 0;
 
-            foreach (string line in calibrationDocument)
+            foreach (string line in fileData)
             {
                 int firstDigit = FindFirstDigit(line);
                 int lastDigit = FindLastDigit(line);
 
-                int calibrationValue = firstDigit * 10 + lastDigit;
-                totalCalibration += calibrationValue;
+                int currentLineValue = firstDigit * 10 + lastDigit;
+                totalValue += currentLineValue;
             }
 
-            return totalCalibration;
+            return totalValue;
         }
 
         private static int FindFirstDigit(string line)

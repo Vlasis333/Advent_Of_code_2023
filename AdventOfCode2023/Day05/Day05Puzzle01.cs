f9003da9c0c91191286907ea6258b0b8e04ccfb4
Vlasios Mavraganis <vlasisvv@gmail.com>
2023-12-06 00:38:29 +0200

    day 05 puzzle 01 completed

--- AdventOfCode2023/Day05/Day05Puzzle01.cs
+++ AdventOfCode2023/Day05/Day05Puzzle01.cs
@@ -1,11 +1,8 @@
-﻿namespace AdventOfCode2023
+﻿using System;
+namespace AdventOfCode2023
 {
-
     public class Day05Puzzle01 : IPuzzle
     {
-        // Used to save all the numbers and find the score
-        private static readonly List<(int[] leftNumbers, int[] rightNumbers)> Cards = new();
-
         public void Calculation()
         {
             string[] puzzleInput = ReadFile(@"C:\Users\vlasi\Desktop\Main Files\Projects\Code Base\Console\AdventOfCode2023\AdventOfCode2023 Inputs\inputDay05.txt");
@@ -18,9 +15,83 @@
             return File.ReadAllLines(filePath);
         }
 
-        private static int CalculateAnswer(string[] fileData)
+        private static long CalculateAnswer(string[] fileData)
         {
-            return 0;
+            List<long> initialSeeds = GetInitialSeeds(fileData);
+            Dictionary<long, long> seedToLocationMapping = GetSeedToLocationMapping(fileData, initialSeeds);
+            List<long> lowestLocations = new ();
+
+            // Spliting string at this point is the same method, only diffrence is where we split
+            foreach (long seed in initialSeeds)
+            {
+                long location = seedToLocationMapping[seed];
+                lowestLocations.Add(location);
+            }
+
+            return lowestLocations.Min();
+        }
+
+        private static Dictionary<long, long> GetSeedToLocationMapping(string[] fileData, List<long> initialSeeds)
+        {
+            string concatenatedData = string.Join("\n", fileData);
+            string[] parts = concatenatedData.Split("\n\n");
+
+            List<string> locationMappings = parts.Skip(1).ToList();
+
+            List<(long, long, long)[]> mappingFunctions = locationMappings
+                .Select(s => s.Split('\n').Skip(1)
+                    .Select(line => line.Split().Select(long.Parse).ToArray())
+                    .Select(arr => (arr[0], arr[1], arr[2]))
+                    .ToArray())
+                .ToList();
+
+            Dictionary<long, long> resultMapping = new ();
+            for (int i = 0; i < initialSeeds.Count; i++)
+            {
+                long currectSeed = initialSeeds[i];
+                foreach (var funcTuples in mappingFunctions)
+                {
+                    currectSeed = ApplyFunctions(currectSeed, funcTuples);
+                }
+                resultMapping.Add(initialSeeds[i], currectSeed);
+            }
+
+            return resultMapping;
+        }
+
+        private static List<long> GetInitialSeeds(string[] fileData)
+        {
+            // Used to get the data from the first row of the input
+            string concatenatedData = string.Join("\n", fileData);
+            string[] parts = concatenatedData.Split("\n\n");
+
+            string seedMappingString = parts[0];
+            return seedMappingString.Split(':')[1]
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(value => long.Parse(value))
+                .ToList();
+        }
+
+        private static long ApplyMappingsInRange(long currectSeed, IEnumerable<(long Destination, long Source, long Size)> mappingTuples)
+        {
+            foreach (var mapping in mappingTuples)
+            {
+                if (IsWithinRange(currectSeed, mapping.Source, mapping.Size))
+                {
+                    currectSeed = ApplyMapping(currectSeed, mapping.Destination, mapping.Source);
+                }
+            }
+            return currectSeed;
+        }
+
+        private static bool IsWithinRange(long value, long start, long size)
+        {
+            return start <= value && value < start + size;
+        }
+
+        private static long ApplyMapping(long originalValue, long destination, long source)
+        {
+            return originalValue + (destination - source);
         }
 
     }
--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -8,7 +8,7 @@
             RunDay("Day 02", new Day02Puzzle01(), new Day02Puzzle02());
             RunDay("Day 03", new Day03Puzzle01(), new Day03Puzzle02());
             RunDay("Day 04", new Day04Puzzle01(), new Day04Puzzle02());
-            //RunDay("Day 05", new Day05Puzzle01(), new Day05Puzzle02());
+            RunDay("Day 05", new Day05Puzzle01(), new Day05Puzzle02());
 
             // Keep the command window open
             Console.ReadLine();
